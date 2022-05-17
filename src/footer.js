9a1680510e9adbc12ddf01a8c20131b9146a9305
hsingla378 <hsingla378@gmail.com>
2022-05-18 00:27:46 +0530

    Umm.. the first one

---
+++ src/footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Footer(){
+    return (
+        <footer id="footer">
+            {/* <a href="https://twitter.com/akahsingla" target="_blank"><i class="fa fa-twitter-square"></i></a> */}
+            <a href="https://twitter.com/akaHSingla" target="_blank"><span>Twitter</span></a>
+            <a href="https://www.facebook.com/hsodhan/" target="_blank"><span>Facebook</span></a>
+            <a href="https://www.instagram.com/hs_aggarwal/" target="_blank"><span>Instagram</span></a>
+            <a href="https://github.com/hsingla378" target="_blank"><span>GitHub</span></a>
+        </footer>
+    )
+}
---
+++ src/main.js
@@ -0,0 +1,31 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import profileImage from "../src/images/himanshu.jpg"
+
+export default function MainContent() {
+    return (
+        <main>
+                        <img src={profileImage} alt="Himanshu Singla" width="100%"></img>
+
+            <div className="mainContent">
+                <h1 id="profileName">Himanshu Singla</h1>
+                <span id="designation">Frontend Developer</span>
+                <a href="https://hsingla378.github.io/" target="_blank"><p id="websiteLink">hsingla378.github.io</p></a>
+
+                <div>
+                    <a href="mailto:hsingla378@gmail.com"><button id="emailButton">Email</button></a>
+                    <a href="https://www.linkedin.com/in/akahimanshusingla" target="_blank"><button id="linkedinButton">Linkedin</button></a>
+                </div>
+
+                <div id="bioContent">
+                    <h2>About</h2>
+                    <p>He is a BCA Student and an early-stage web developer. 🙂 He believes in Sharing is Caring and is always curious to help and interact with new people. 🤗
+                    </p>
+
+                    <h2>Interest</h2>
+                    <p>Badminton Lover. Coffee fanatic. Sleepy. Internet fanatic. Music scholar</p>
+                </div>
+            </div>
+        </main>
+    )
+}
